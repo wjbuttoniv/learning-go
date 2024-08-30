f59a04566220940b46bf1ee0ebfdfbb347ca74a2
johnnybeedev <wjbuttoniv@gmail.com>
2024-08-30 13:44:37 -0400

    init commit demonstrating type error

---
+++ Chapter-1-Why-Write-Go/basic-auth.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var username string = "wagslane"
+	var password int = 20947382822
+
+	// Do not edit below this line
+
+	fmt.Println("Authorization: Basic", username+":"+password)
+}
+
+
+
