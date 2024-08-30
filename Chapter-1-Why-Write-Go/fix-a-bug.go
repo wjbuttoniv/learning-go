2c48c3dd3998fb94f0e5570a54dbb4b30a7c472f
johnnybeedev <wjbuttoniv@gmail.com>
2024-08-30 07:37:00 -0400

    corrected bug from c5b7d157
    bug fix: incorrect order of operations used

--- Chapter-1-Why-Write-Go/fix-a-bug.go
+++ Chapter-1-Why-Write-Go/fix-a-bug.go
@@ -13,15 +13,9 @@ func main() {
 numMessages := float64(len(messageFromDoris))
   costPerMessage := 0.02
 
-// do not make any changes above
-
-// Line Below is the bug
-  // Customer States:
-  // "We are being charged inaccurately"
-  // "The cost per message is 0.02"
-totalCost := costPerMessage + numMessages
-
-// do not make any changes below
+// Corrected bug from commit c5b7d157 
+  // incorrect operation of addition instead of multiplication
+totalCost := costPerMessage * numMessages
 
 fmt.Printf("Doris spent %.2f on messages today\n", totalCost)
 
