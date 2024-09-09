51bf2aa9f5e074099a63e487cc903d1d00a9b650
johnnybeedev <lucy@edgerunner.localdomain>
2024-09-09 08:50:40 -0400

    resolved bug from 3866ea8c

--- Chapter-3-Functions/vars-by-value.go
+++ Chapter-3-Functions/vars-by-value.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
+// Alter the below to capture the return value of incrementSends
+// and overwrite previous sendsSoFar value
 func main() {
   sendsSoFar := 430
   const sendsToAdd = 25
-  incrementSends(sendsSoFar, sendsToAdd)
+  sendsSoFar = incrementSends(sendsSoFar, sendsToAdd)
   fmt.Println("you've sent", sendsSoFar, "messages")
 }
 
+// Alter the below to increment sendsSoFar before returning
 func incrementSends(sendsSoFar, sendsToAdd int) int {
+  sendsSoFar++
   sendsSoFar = sendsSoFar + sendsToAdd
+  return sendsSoFar
 }
