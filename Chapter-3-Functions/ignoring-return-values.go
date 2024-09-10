6c1af1badaf846f7cb4d197b341a5697fccc6dae
johnnybeedev <lucy@edgerunner.localdomain>
2024-09-10 08:08:22 -0400

    textio now addresses user by first name

--- Chapter-3-Functions/ignoring-return-values.go
+++ Chapter-3-Functions/ignoring-return-values.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
+// only return the customers firstName
 func main() {
-	firstName, lastName := getNames()
+	firstName, _ := getNames()
 	fmt.Println("Welcome to Textio", firstName)
 }
 
 // dont edit below this line
-
-func getNames(string, string) {
+// below func syntax was incorrect in init commit
+func getNames() (string, string) {
 	return "John", "Doe"
 }
