940f7674c682fd6d44e219ff6e5fef19f078b942
installer <installer@gmail.com>
2024-09-11 14:35:21 -0400

    init commit

---
+++ Chapter-4-Structs/struct-basics.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Complete the below struct
+type messageToSend struct {
+	phoneNumber int
+	message     string
+}
+
+// dont edit below this line
+
+func test(m messageToSend) {
+	fmt.Printf("Sending message: '%s' to:%v\n", m.message, m.phoneNumber)
+	fmt.Println("===============================")
+}
+
+func main() {
+	test(messageToSend{
+		phoneNumber: 148255510981,
+		message:     "Thanks for signing up",
+	})
+	test(messageToSend{
+		phoneNumber: 148255510982,
+		message:     "Love to have you aboard!",
+	})
+	test(messageToSend{
+		phoneNumber: 148255510983,
+		message:     "And now a message from our sponser...",
+	})
+}
