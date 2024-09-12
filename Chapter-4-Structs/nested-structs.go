c2887d8d48364b44b4e6faca8348f432711a7432
johnnybeedev <wjbuttoniv@gmail.com>
2024-09-12 07:02:52 -0400

    completed assignment

--- Chapter-4-Structs/nested-structs.go
+++ Chapter-4-Structs/nested-structs.go
@@ -1,11 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Assignment: Need to verify required fields
 // have non-zero values
 type messageToSend struct {
-	message  string
-	sender   user
-	recipent user
+	message   string
+	sender    user
+	recipient user
 }
 
 type user struct {
@@ -15,14 +19,80 @@ type user struct {
 
 // Complete the below function
 // should only return true if
-// sender and recipent each contain
+// sender and recipient each contain
 // a name and number
 // else return false
 func canSendMessage(mToSend messageToSend) bool {
+	if mToSend.sender.name == "" || mToSend.sender.number == 0 {
+		return false
+	}
+	if mToSend.recipient.name == "" || mToSend.recipient.number == 0 {
+		return false
+	}
 	return true
 }
 
 // dont touch below this line
 
 func test(mToSend messageToSend) {
+	fmt.Printf(`sending "%s" from %s (%v) to %s (%v)...`,
+		mToSend.message,
+		mToSend.sender.name,
+		mToSend.sender.number,
+		mToSend.recipient.name,
+		mToSend.recipient.number,
+	)
+	fmt.Println()
+	if canSendMessage(mToSend) {
+		fmt.Println("...sent!")
+	} else {
+		fmt.Println("...can't send message")
+	}
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(messageToSend{
+		message: "you have an appointment tommorow",
+		sender: user{
+			name:   "Brenda Halafax",
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Sally Sue",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have an event tommorow",
+		sender: user{
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Suzie Sall",
+			number: 0,
+		},
+	})
+	test(messageToSend{
+		message: "you have an party tommorow",
+		sender: user{
+			name:   "Njorn Halafax",
+			number: 16545550987,
+		},
+		recipient: user{
+			name:   "Sally Sue",
+			number: 19035558973,
+		},
+	})
+	test(messageToSend{
+		message: "you have a birthday tommorow",
+		sender: user{
+			name:   "Eli Halafax",
+			number: 0,
+		},
+		recipient: user{
+			name:   "Whitaker Sue",
+			number: 19035558973,
+		},
+	})
 }
