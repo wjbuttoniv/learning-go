6dd05683ce79e3c870eabdc92eeb1a71164ba36c
johnnybeedev <wjbuttoniv@gmail.com>
2024-09-13 08:14:01 -0400

    init commit

---
+++ Chapter-5-Interfaces/interface-implementation.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+type employee interface {
+	getName() string
+	getSalary() int
+}
+
+type contractor struct {
+	name         string
+	hourlyPay    int
+	hoursPerYear int
+}
+
+func (c contractor) getName() string {
+	return c.name
+}
+
+// ?
+
+// dont touch below this line
+
+type fullTime struct {
+	name   string
+	salary int
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
+
+func test(e employee) {
+	fmt.Println(e.getName(), e.getSalary())
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(fullTime{
+		name:   "Jack",
+		salary: 5000,
+	})
+	test(contractor{
+		name:         "Bob",
+		hourlyPay:    100,
+		hoursPerYear: 73,
+	})
+	test(contractor{
+		name:         "Jill",
+		hourlyPay:    872,
+		hoursPerYear: 982,
+	})
+}
