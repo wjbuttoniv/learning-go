d18e101b444a699f232912d7f04633e790725d6e
johnnybeedev <wjbuttoniv@gmail.com>
2024-09-16 08:45:25 -0400

    intit commit, assignment complete

---
+++ boot-dev-go/Chapter-5-Interfaces/multiple interfaces/multiple-interfaces.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Assignment
+// add the required methods so that the email type
+// implements both the expense and printer interfaces
+
+// if the email is not Subscribed, cost is 0.05 per char
+// if it is Subscribed, cost is 0.01 per char
+func (e email) cost() float64 {
+	subCost := 0.01
+	stdCost := 0.05
+	if e.isSubscribed == true {
+		return subCost * float64(len(e.body))
+	}
+	return stdCost * float64(len(e.body))
+}
+
+// print method should print emails body text
+// to standard out
+func (e email) print() {
+	fmt.Println(e.body)
+}
+
+type expense interface {
+	cost() float64
+}
+
+type printer interface {
+	print()
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+}
+
+func print(p printer) {
+	p.print()
+}
+
+func test(e expense, p printer) {
+	fmt.Printf("Printing with cost: $%.2f ...\n", e.cost())
+	p.print()
+	fmt.Println("======================================")
+}
+
+func main() {
+	e := email{
+		isSubscribed: true,
+		body:         "hello there",
+	}
+	test(e, e)
+	e = email{
+		isSubscribed: false,
+		body:         "I want my money back",
+	}
+	test(e, e)
+	e = email{
+		isSubscribed: true,
+		body:         "Are you free for a chat?",
+	}
+	test(e, e)
+	e = email{
+		isSubscribed: false,
+		body:         "This meeting could have been an email",
+	}
+	test(e, e)
+}
