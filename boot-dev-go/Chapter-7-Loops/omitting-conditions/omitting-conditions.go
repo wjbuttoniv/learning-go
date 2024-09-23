f7d6efb4680b0cbadfd12dd7ee207669f5ffddf7
johnnybeedev <wjbuttoniv@gmail.com>
2024-09-23 14:21:16 -0400

    batman

---
+++ boot-dev-go/Chapter-7-Loops/omitting-conditions/omitting-conditions.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func maxMessages(thresh float64) int {
+	// ?
+}
+
+func test(thresh float64) {
+	fmt.Printf("Threshold: %.2f\n", thresh)
+	max := maxMessages(thresh)
+	fmt.Printf("Maximum messages that can be sent: = %v\n", max)
+	fmt.Println("===================================================")
+}
+
+func main() {
+	test(10.00)
+	test(20.00)
+	test(30.00)
+	test(40.00)
+	test(50.00)
+}
