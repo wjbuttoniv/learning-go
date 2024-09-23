390b70243ea5e552cc0fa963ba3e364c8c8909dd
johnnybeedev <wjbuttoniv@gmail.com>
2024-09-23 18:32:31 -0400

    assignment complete, gathering additional feedback

--- boot-dev-go/Chapter-7-Loops/omitting-conditions/omitting-conditions.go
+++ boot-dev-go/Chapter-7-Loops/omitting-conditions/omitting-conditions.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
+// Assignment
+// calculate the maximum number of messages that can be sent
+// given a threshold
 func maxMessages(thresh float64) int {
-	// ?
+	totalCost := 0.0
+	for i := 0; ; i++ {
+		totalCost += 1.0 + (0.01 * float64(i))
+		if totalCost > thresh {
+			return i
+		}
+	}
 }
 
 func test(thresh float64) {
