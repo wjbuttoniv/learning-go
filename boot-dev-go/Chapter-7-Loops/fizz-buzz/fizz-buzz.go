1d6ce9e4048963244ca084d3eee7fb87f661acd0
johnnybeedev <wjbuttoniv@gmail.com>
2024-09-25 09:55:17 -0400

    assignment complete

--- boot-dev-go/Chapter-7-Loops/fizz-buzz/fizz-buzz.go
+++ boot-dev-go/Chapter-7-Loops/fizz-buzz/fizz-buzz.go
@@ -1,7 +1,26 @@
 package main
 
+import "fmt"
+
+// Assignment
+// Complete the below function
+// print number 1-100
+// if
+// -	multiple of 3, print fizz
+// -	multiple of 5, print buzz
+// -	both, print fizzbuzz
 func fizzbuzz() {
-	// ?
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%5 == 0 && i%3 != 0 {
+			fmt.Println("buzz")
+		} else if i%3 == 0 && i%5 != 0 {
+			fmt.Println("fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
 
 func main() {
